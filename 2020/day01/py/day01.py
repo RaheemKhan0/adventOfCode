bfc8a9248a200a4de5235049c97db22d16ec6149
Muhammad Raheem Khan <muhammadraheemk96@gmail.com>
2025-11-04 15:20:50 +0000

    solved the first day

---
+++ 2020/day01/py/day01.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+HERE = Path(__file__).resolve()
+DAY_DIR = HERE.parent.parent
+
+
+
+values = list()
+with open(f"{DAY_DIR}/input.txt", 'r') as inputs:
+    for val in inputs:
+        values.append(int(val))
+print('values ' , values)
+
+for i in range(len(values)):
+    for j in range(i, len(values)):
+        for z in range(j, len(values)):
+            if values[i] + values[j] + values[z] == 2020:
+                print(values[i] * values[j] * values[z])
+                break
+
+
+
