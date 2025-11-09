f7c57fc1368a8054ff6925ca65811aaa9eb1a481
Muhammad Raheem Khan <muhammadraheemk96@gmail.com>
2025-11-09 21:53:54 +0000

    added the part 1 solution for day 4

--- 2020/day04/js/main.js
+++ 2020/day04/js/main.js
@@ -5,18 +5,38 @@ import { fileURLToPath } from "url";
 const fileName = fileURLToPath(import.meta.url);
 const dirName = path.dirname(fileName);
 
-const inputs = readFileSync(path.join(dirName, '../day04.txt'), 'utf-8');
-console.log('inputs : ', inputs);
-const arr = inputs.split('\n\n');
+const inputs = readFileSync(path.join(dirName, "../day04.txt"), "utf-8");
+const arr = inputs.split("\n\n");
 
+const requiredarr = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+let totalvalid = 0;
 for (let str of arr) {
-  str = str.replace('\n', ' ');
-  const refinedarr = str.split(' ');
-  console.log('refined arr : ', refinedarr);
-  
-
+  const requiredMap = new Map();
+  for (const req of requiredarr) {
+    requiredMap.set(str, false);
+  }
+  str = str.replace(/\n/g, " ");
+  const refinedarr = str.split(" ");
+  for (let prop of refinedarr) {
+    if (prop != "") {
+      const temp = prop.split(":");
+      requiredMap.set(temp[0], true);
+    }
+  }
+  if (isvalid(requiredMap, requiredarr)) {
+    totalvalid++;
+  }
 }
+console.log('total valid : ', totalvalid);
 
-
-
-
+function isvalid(reqMap, reqArr) {
+  console.log('Entering isvalid function : ');
+  console.log('req map : ', reqMap);
+  console.log('req arr : ', reqArr);
+  for (const req of reqArr) {
+    if (!reqMap.get(req)) {
+      return false;
+    }
+  }
+  return true;
+}
