776a10d92dbabdde022ccf79dc736bb5da0ad2ed
Muhammad Raheem Khan <muhammadraheemk96@gmail.com>
2026-01-13 22:58:10 +0000

    added log

--- 2025/day01/js/main.js
+++ 2025/day01/js/main.js
@@ -2,6 +2,7 @@ import { readFileSync } from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
+console.log('hello');
 const __filename = fileURLToPath(import.meta.url);
 const dir = path.dirname(__filename);
 const inputs = readFileSync(path.join(dir, "../day01.txt"), "utf8");
